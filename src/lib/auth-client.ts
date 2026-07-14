38f2a6fa9dc95b1080b5ec5d5847da904b04f019
Zé Diogo Ribeiro <josediogo@significa.co>
2026-07-14 21:11:24 +0100

    fix: auth url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,9 @@
-import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
+
+const url = process.env.BETTER_AUTH_URL;
 
 export const authClient = createAuthClient({
-	baseURL: "http://localhost:3000",
+	baseURL: url,
 	plugins: [adminClient()],
 });
